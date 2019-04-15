854024067ead87f908428558f7a3ae74acb25a23
Mario Saso <43787171+M4R1OS4S0@users.noreply.github.com>
2019-04-15 02:18:05 +0200

    Update mirror-enc.py

--- mirror-enc.py
+++ mirror-enc.py
@@ -1,76 +1,64 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 
 import sys
-import sources.module_files as module_files
-import sources.module_texts as module_texts
+import modules.module_file as module_file
+import modules.module_text as module_text
+import modules.module_gen as module_gen
 
-
-def banner():
-    
+def banner():   
     banner = """
     
      ___________ @ @                                                     
-    /   v1.0   @\   @                       _                            
+    /          @\   @                       _                            
     \___________/  _@              ___ __ _(_)__ _ __ _  ___ __ _        
               @  _/@ \_____       / _ ' _` | |__` |__` |/ _ \__` |       
                @/ \__/-=-=^`     | | | | | | |  | |  | | (_) | | |       
                 \_ /             |_| |_| |_|_|  |_|  |_|\___/  |_|-enc   
                  <|                                                      
                  <|                    github.com/M4R1OS4S0              
-                 <|                                                     
+                 <|
+                 
     """
-
     print(banner)
 
-
-def help():
-
-    help = """
-    Usage:
-
-    python mirror-enc.py -f FILE or FOLDER (1), FILE or FOLDER (2), ... , FILE or FOLDER (n)    # FILES MODE
-
-    python mirror-enc.py -t TEXT (1), TEXT (2), ... , TEXT (n)                                  # TEXTS MODE
-    
+def initial():
+    menu = """
+    [1.] TEXT Mode  [ RSA, AES, Hash and Base64 ]  
+    [2.] FILE Mode  [ AES, Base64 and Hash ]
+    [3.] GEN Mode   [ AES and RSA keypair ]   
+    [CTRL + C] EXIT mirror-enc  
     """
-
-    modes = ['-f', '-t']
-
-    if len(sys.argv) == 1 or sys.argv[1] not in modes:
-        print(help)
-        sys.exit()
-
-
+    print(menu)
+    x = '0'
+    while x not in ['1', '2', '3']:
+        x = str(raw_input("mirror-enc > "))
+    if x == '1':
+        mode = 't'
+    if x == '2':
+        mode = 'f'
+    if x == '3':
+        mode = 'g'
+    return mode
 
 def main():
+    banner()
+    var = 'N'
+    while var.upper() == 'N':
+        var = ''
+        mode = initial()
+        if mode == 'f':
+            module_file.main(mode)
+        elif mode == 't':
+            module_text.main(mode)
+        elif mode == 'g':
+            module_gen.main(mode)
+        while var.upper() != 'Y' and var.upper() != 'N':
+            var = str(raw_input("\nDo you want to exit? (Y/N): "))
 
+if __name__ == '__main__':
     try:
-
-        banner()
-        help()
-
-        if sys.argv[1] == '-f':
-            module_files.main(sys.argv)
-
-        elif sys.argv[1] == '-t':
-            module_texts.main(sys.argv)
-
-        print('')
-
+        main()
     except KeyboardInterrupt:
         print('\n')
         sys.exit()
-
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-    
-
-
-
